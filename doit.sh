1ddb29750fc431dbff268aae066b2702e4eb8d4a
Damien Cavanagh <damcav@users.noreply.github.com>
2018-10-09 15:17:12 +0100

    curl

---
+++ doit.sh
@@ -0,0 +1,2 @@
+curl -v http://localhost/mobile/MobileSession/PPLoginV3 \
+--header 'User-Agent: Apple 5.1.' \--header 'Content-Type: text/xml' \-d '<Credentials><LoginID>XXXXXX@outtask.com</LoginID><Password>XXXXXX</Password></Credentials>''http://localhost/mobile/MobileSession/PPLoginV3'
